db0c4b329200047c922bb3e6d4d2f634f2300306
LukaszWisniewski123 <lukasz9wisniewski@gmail.com>
2016-12-20 14:15:29 +0100

    Zad1

--- Zad1/src/Zad1/Program.cs
+++ Zad1/src/Zad1/Program.cs
@@ -4,53 +4,54 @@ namespace Zad1
 {
     public class Program
     {
-        public static void Main(params string[] args) 
+        static void Main(string[] args) 
         {
-            
             DirectoryInfo filesInformations;
             DateTime time = new DateTime();
-            
-            string[] fileList; 
-            string directory = Directory.GetCurrentDirectory(); 
+
+            string[] fileList;
+            string directory = "";
 
             try
             {
-                Console.WriteLine("Pierwszy argument aplikajci: {0}\n\n", args[0]); 
+                directory = Path.GetDirectoryName(args[0]);
+                Console.WriteLine("katalog args[0] {0}\n\n", directory);
             }
-            catch(IndexOutOfRangeException)
+            catch (IndexOutOfRangeException)
             {
-                string[] Args = Environment.GetCommandLineArgs(); 
-                Console.WriteLine("Pierwszy argument aplikajci: {0}\n\n", Args[0]); 
+                args = Environment.GetCommandLineArgs();
+                directory = Path.GetDirectoryName(args[0]);
+                Console.WriteLine("katalog args[0] {0}\n\n", directory);
             }
-            
-            if (Directory.Exists(directory)) 
+
+            if (Directory.Exists(directory))
             {
                 Console.WriteLine("Katalog istnieje");
-                fileList = Directory.GetFiles(directory); 
-               
+                fileList = Directory.GetFiles(directory);
+
                 foreach (string file in fileList)
-                {   
-                    filesInformations = new DirectoryInfo(file); 
+                {
+                    filesInformations = new DirectoryInfo(file);
                     time = filesInformations.CreationTime;
                     time.ToLocalTime();
                     Console.Write("+Nazwa pliku: ");
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(""+filesInformations.Name);
+                    Console.WriteLine("" + filesInformations.Name);
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(" -Rozszerzenie: ");
+                    Console.Write("--Rozszerzenie: ");
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("" + filesInformations.Extension);
+                    Console.WriteLine("*" + filesInformations.Extension);
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(" -Sciezka: ");
+                    Console.Write("--Sciezka: ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("" + file);
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(" -Data utworzenia: ");
+                    Console.Write("--Data utworzenia: ");
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("" + time);
                     Console.ForegroundColor = ConsoleColor.Gray;
-                }          
-            }  
+                }
+            }
             else
                 Console.WriteLine("katalog nie istnieje.");
         }
